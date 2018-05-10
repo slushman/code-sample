62afdc5bbab3770c9e303b3ef30a388100de286d
Chris Wilcoxson <chris@slushman.com>
2018-05-10 11:13:28 -0500

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,53 @@
+=== Code Sample ===
+Contributors: slushman
+Donate link: https://www.slushman.com/
+Tags: editor, code
+Requires at least: 3.0.1
+Tested up to: 4.9.6
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds the Code Sample TinyMCE plugin to the editor.
+
+
+
+== Description ==
+
+Adds the Code Sample TinyMCE plugin to the editor.
+
+The Code Sample plugin for TinyMCE is developer by TinyMCE. This plugin adds a code sample button, 
+which brings up a modal window where the user can enter their code sample and select the language. The 
+plugin inserts the code sample wrapped in <pre> and <code> tags.
+
+This plugin does not include syntax highlighting.
+
+
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+
+
+== Frequently Asked Questions ==
+
+= Is syntax highlighting included? =
+
+No, there is no syntax highlighting included here. Just the Code Sample plugin and the button.
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Plugin published.
+
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Plugin published.
---
+++ code-sample.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Plugin Name: 			Code Sample
+ * Plugin URI: 				https://github.com/slushman/code-sample/
+ * Description: 			Adss a code sample button to the TinyMCE Editor.
+ * Author: 					slushman
+ * Author URI: 				https://www.slushman.com/
+ * GitHub Plugin URI: 		https://github.com/slushman/code-sample/
+ * Github Branch: 			master
+ * Version: 				1.0.0
+ * License: 				GPL2+
+ * License URI: 			http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @since 					1.0.0
+ * @package 				Code_Sample
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+add_filter( 'mce_external_plugins', 'code_sample_add_external_plugin', 999, 1 );
+add_filter( 'mce_buttons', 'code_sample_button', 999, 1 );
+add_action( 'admin_enqueue_scripts', 'code_sample_scripts' );
+
+/**
+ * Adds the Code Sample TinyMCE plugin from the CloudFlare CDN to
+ * the list of MCE plugins.
+ * 
+ * @hooked 		mce_external_plugins
+ * @since 		1.0.0
+ * @param 		array 		$mce_plugins 		The current MCE plugins array.
+ * @return 		array 		$mce_plugins 		The modified MCE plugins array.
+ */
+function code_sample_add_external_plugin( $mce_plugins ) {
+
+	$mce_plugins['codesample'] = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.12/plugins/codesample/plugin.min.js'; // 
+
+	return $mce_plugins;
+
+} // code_sample_add_external_plugin()
+
+/**
+ * Adds the Code Sample button to the list of buttons
+ * for the first row of the TinyMCE editor.
+ * 
+ * @hooked 		mce_buttons
+ * @since 		1.0.0
+ * @param 		array 		$mce_buttons 		The current MCE buttons array.
+ * @return 		array 		$mce_plugins 		The modified MCE buttons array.
+ */
+function code_sample_button( $mce_buttons ) {
+
+	$mce_buttons[] = 'codesample';
+
+	return $mce_buttons;
+
+} // code_sample_button
+
+/**
+ * Enqueues the init script to initialize the Code Sample TinyMCE plugin.
+ * 
+ * @since 		1.0.0
+ */
+function code_sample_scripts() {
+
+	wp_enqueue_script( 'code-sample-init', plugin_dir_url( __FILE__ ) . 'js/code-sample-init.js', array(), '1.0.0', true );
+
+} // code_sample_scripts()
